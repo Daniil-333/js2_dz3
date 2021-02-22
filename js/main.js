ec288729b3d6d626e558c6dd66513b8054346581
Daniel <monobootica@yandex.ru>
2021-02-22 08:24:05 +0500

    dz3 is failed

--- js/main.js
+++ js/main.js
@@ -19,6 +19,19 @@ let getRequest = (url, cb) => {
     }
 };
 
+let getRequestNew = new Promise((resoleve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'url', true);
+    xhr.onload = () => resoleve(xhr.response);
+    xhr.onerror = () => reject(new Error('Запрос завершился неудачей'));
+    xhr.send();
+});
+    getRequestNew.then((xhr) => {
+        console.log(xhr.responseText);
+    });
+    getRequestNew.catch((error) => {
+        console.log('some error');
+    });
 
 class Products {
     products = [];
@@ -97,7 +110,6 @@ class Cart {
 
     init() {
         this._showHideCart('.btn-cart');
-        this._addItem('.buy-btn');
     }
 
     fetchItems() {
@@ -107,16 +119,20 @@ class Cart {
                 for (let item of data.contents) {
                     this.items.push(new CartItem(item));
                 }
-                this.amount = data.amount;
-                this.countGoods = data.countGoods;
+                //Подсчёт общей стоимости и количества товаров в корзине
+                this.amount = this.items.reduce((accum, item) => accum += item.unitPrice, 0);
+                this.countGoods = this.items.reduce((accum, item) => accum += item.quantity, 0);
             })
     }
 
     renderCart() {
         for (let item of this.items) {
+            if (item.rendered) {
+                continue;
+            }
             this.itemsContainer.insertAdjacentHTML('beforeend', item.render())
         }
-         this.itemsContainer.insertAdjacentHTML('beforeend', 
+         this.itemsContainer.insertAdjacentHTML('beforeend',
                 `<div class="general">
                 <p>Количество товаров в корзине: ${this.countGoods}</p>
                 <p>Общая сумма товаров в корзине: ${this.amount}</p>
@@ -126,21 +142,28 @@ class Cart {
     _showHideCart(selector) {
         document.querySelector(selector).addEventListener('click', () => {
             document.querySelector('.cart').classList.toggle('hidden');
-        })
+            this._removeItem('.del-btn');
+        });
     }
 
-    _addItem(selector) {
-        let btnItems = document.querySelectorAll(selector);
-        for (let btnItem of btnItems) {
-            btnItem.addEventListener('click', () => this.items.push(new CartItem(item)))
-          }
+    //Ничего не получилось. По началу удалял сами элементы товаров по нажатию на кнопку. Но в таком случае стоимость и количество не пересчитавались. Подумал, что нужно убирать элементы в массиве. Элементы убираются, а разметка остаётся такой же.
+    _removeItem(selector) {
+        if (!document.querySelector('.cart').classList.contains('hidden')) {
+            let delBtn = document.querySelectorAll(selector);
+            for (let btn of delBtn) {
+                btn.addEventListener('click', (event) => {
+                    console.log(event.target.parentNode.dataset.id);
+                    this.items = this.items.filter(cartItem => cartItem.id !== Number(event.target.parentNode.dataset.id));
+                })
+            }
+        }
     }
-    // removeItem() - удаление одного товара из корзины
+
+    // _addItem(selector) {}
     // clear() - очистка корзины
     // makePercentDiscount(arg) - применение скидки, в качестве аргумента раззмер скидки в процентах
     // subtotal() - промежуточная стоимость всех товаров корзины(до применения скидки, если таковая имеется)
     // grandTotal() - итоговая стоимость всех товаров корзины
-
 }
 
 class CartItem {
@@ -148,14 +171,17 @@ class CartItem {
     img = '';
     unitPrice = 0;
     quantity = 0;
+    id = 0;
+    rendered = false;
 
     constructor(item, img = 'https://placehold.it/100x75') {
-        ({product_name: this.name, price: this.unitPrice, quantity: this.quantity} = item);
+        ({product_name: this.name, price: this.unitPrice, quantity: this.quantity, id_product: this.id} = item);
         this.img = img;
     }
 
     render() {
-        return `<div class="cart-item">
+        this.rendered = true;
+        return `<div class="cart-item" data-id="${this.id}">
                  <img src="${this.img}" alt="${this.name}">
                  <div class="descItem">
                      <h3>${this.name}</h3>
